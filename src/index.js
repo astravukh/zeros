dc08793b0594e1194cc1a846897b0eb1b24dbd3d
Maksim Astravukh <astravukh@gmail.com>
2018-09-14 08:18:15 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  var count5 = 0;
+  for(var i=1; i<=number; i++){    
+    var j = i;
+    while(j%5 == 0){
+      j = j/5;
+      count5++;
+    }
+  }
+  return count5;
 }
